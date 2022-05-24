e59e8a47121ce1c4adf36a7695b7569232cc1b99
Daniel Yang <danyang0724@gmail.com>
2022-05-24 10:19:13 -0400

    Created ability to push task to li and to delete them

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let form = document.querySelector("#create-task-form")
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const taskDescription = e.target["new-task-description"].value;
+    createNewTask(taskDescription);
+    form.reset();
+  })
 });
+
+function createNewTask(taskDescription) {
+  let ul = document.getElementById("tasks")
+  let li = document.createElement("li");
+  let btn = document.createElement("button");
+  btn.addEventListener("click", deleteTask)
+  btn.textContent = " x"
+  li.textContent = `${taskDescription} `
+  li.append(btn)
+  ul.append(li)
+}
+
+function deleteTask(e) {
+  e.target.parentNode.remove();
+}
